9953a35335f3b2d8399e81c33e04ef39082c2b4f
Davidvl <josuedavidvl18@gmail.com>
2023-07-31 03:11:37 -0600

    Rutas terminadas y app lista para probar, sin estilos css

--- src/main.js
+++ src/main.js
@@ -1,13 +1,28 @@
 import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
+import VueRouter from 'vue-router';
+import WeightForm from './components/WeightForm.vue';
+import WeightChart from './components/WeightChart.vue';
 
 Vue.config.productionTip = false;
 
+Vue.use(VueRouter);
+
+const routes = [
+  { path: '/registro', component: WeightForm },
+  { path: '/grafico', component: WeightChart },
+];
+
+const router = new VueRouter({
+  routes,
+});
+
 new Vue({
-    store,
-    created() {
-        this.$store.dispatch('initializeWeightData');
-    },
-    render: (h) => h(App),
+  store,
+  router,
+  created() {
+    this.$store.dispatch('initializeWeightData');
+  },
+  render: (h) => h(App),
 }).$mount('#app');
