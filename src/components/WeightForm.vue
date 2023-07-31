83ab3d3e51f4283466751e2595095bbdc1dea064
Davidvl <josuedavidvl18@gmail.com>
2023-07-31 02:43:17 -0600

    Creando componentes

---
+++ src/components/WeightForm.vue
@@ -0,0 +1,24 @@
+<template lang="">
+    <div>
+        <label for="weightInput">Ingresa tu peso: </label>
+        <input type="number" id="weightInput" v-model="weight" />
+        <button @click="addWeight">Registrar</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            weight: null,
+        };
+    },
+    methods: {
+        addWeight() {
+            if (!this.weight) return;
+            this.$store.commit('ADD_WEIGHT', parseFloat(this.weight));
+            this.weight = null;
+        },
+    },
+};
+</script>
