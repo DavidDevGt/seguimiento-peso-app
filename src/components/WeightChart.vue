2ab600c5ee4faa91c6fddbf6766695336e0fd3c2
Davidvl <josuedavidvl18@gmail.com>
2023-07-31 02:49:43 -0600

    Componentes creados

---
+++ src/components/WeightChart.vue
@@ -0,0 +1,32 @@
+<template lang="">
+    <div>
+        <h2>Gráfico de Progreso de Peso</h2>
+        <canvas ref="chartCanvas"></canvas>
+    </div>
+</template>
+
+<script>
+export default {
+    extends: Line,
+    computed: {
+        weightData() {
+            return this.$store.state.weightData;
+        },
+    },
+    mounted() {
+        this.renderChart(
+            {
+                labels: this.weightData.map((entry, index) => index + 1),
+                datasets: [
+                    {
+                        label: 'Peso',
+                        borderColor: '#41B883',
+                        data: this.weightData,
+                    },
+                ],
+            },
+            { responsive: true, maintainAspectRatio:  false }
+        );
+    },
+};
+</script>
